aa272dca5f586d6a3aba6d76bd180fdcfe6448ea
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-03-07 12:33:59 +0600

    Footer ok

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
-    <div>Footer</div>
-  )
+    <div className="box md:h-[90px] h-[51px] bg-[#000000] w-full flex items-center justify-center">
+      <div className="md:h-[90px] h-[51px] mr-[20px] flex items-center justify-end ">
+        <p className="md:text-3xl text-[12px]">
+          2019-{currentYear} | Designed by Henry francis and Codded by Daniel
+          Asakpa
+        </p>
+      </div>
+    </div>
+  );
 }
